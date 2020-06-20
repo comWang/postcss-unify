87c826710247ceb06a0cf6a2ac2f64ae1098b4c2
wangli <785619377@qq.com>
2020-06-20 15:54:38 +0800

    fix:修复了多参数下的问题

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-unify', function (opts) {
     opts = opts || {};
-    var size = opts.size || 750;
+    var size = opts.size || 700;
     return function (root) {
         root.walkRules(function (rule) {
             var filter = /.*(width)|(height)|(font-size)|(margin)|(padding)|(left)|(right)|(top)|(bottom).*/;
             rule.walkDecls(filter, function (decl) {
-                var value = decl.value.replace(/\s/g, '');
-                if (value * 1 !== 0 && !/\D/.test(value)) {
-                    decl.value = value * 100 / size + 'vw';
+                var value = decl.value.replace(/^\s+|\s+$/g, '');
+                var perList = value.split(' ');
+                var computed = '';
+                if (perList.length === 1 && /^-?\d+$/.test(perList[0])) {
+                    decl.value = parseInt(perList[0]) === 0 ? 0 : perList[0] * 100 / size + 'vw';
+                    return;
                 }
+                computed = perList.reduce(function(accumulator, per) {
+                    if (/^-?\d+$/.test(per)) {
+                        accumulator += ' ' + parseInt(per) === 0 ? 0 : ' ' + per * 100 / size + 'vw';
+                    } else {
+                        accumulator += ' ' + per;
+                    }
+                    return accumulator;
+                }, '')
+                decl.value = computed;
             });
         });
     };
